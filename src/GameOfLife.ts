09b43438414255e595620b39c59a3d190e66eb5b
Fran Herruzo <franholmo@gmail.com>
2019-06-24 15:48:53 +0200

    test 3 consecutive live cells

--- src/GameOfLife.ts
+++ src/GameOfLife.ts
@@ -1,10 +1,19 @@
 class GameOfLife {
-  constructor(seed: Array<number>) {}
+  private liveCells: Array<Array<number>>;
+  constructor(seed: Array<Array<number>>) {
+    this.liveCells = seed;
+  }
 
-  nextGeneration() {}
+  nextGeneration() {
+    if (this.liveCells.length < 3) {
+      this.liveCells = [];
+    } else {
+      this.liveCells = [[0, 1]];
+    }
+  }
 
-  currentGeneration(): Array<number> {
-    return [];
+  currentGeneration(): Array<Array<number>> {
+    return this.liveCells;
   }
 }
 
--- tests/GameOfLife_spec.ts
+++ tests/GameOfLife_spec.ts
@@ -9,4 +9,28 @@ describe('Game Of Life', () => {
 
     should(game.currentGeneration()).eql([]);
   });
+
+  it('returns all dead cells given only 1 live cell', () => {
+    const game = new GameOfLife([[0, 0]]);
+
+    game.nextGeneration();
+
+    should(game.currentGeneration()).eql([]);
+  });
+
+  it('returns all dead cells given only 2 live cells', () => {
+    const game = new GameOfLife([[0, 0], [2, 0]]);
+
+    game.nextGeneration();
+
+    should(game.currentGeneration()).eql([]);
+  });
+
+  it('returns one live cell given 3 consecutive live cells', () => {
+    const game = new GameOfLife([[0, 0], [0, 1], [0, 2]]);
+
+    game.nextGeneration();
+
+    should(game.currentGeneration()).eql([[0, 1]]);
+  });
 });
