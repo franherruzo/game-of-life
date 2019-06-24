a1166471f3c752e137fcd5748ac395c8d6548ceb
Fran Herruzo <franholmo@gmail.com>
2019-06-24 15:25:04 +0200

    Test all cells are dead all continues dead in the next generation

---
+++ src/GameOfLife.ts
@@ -0,0 +1,11 @@
+class GameOfLife {
+  constructor(seed: Array<number>) {}
+
+  nextGeneration() {}
+
+  currentGeneration(): Array<number> {
+    return [];
+  }
+}
+
+export default GameOfLife;
--- tests/GameOfLife_spec.ts
+++ tests/GameOfLife_spec.ts
@@ -1,6 +1,12 @@
+import should from 'should';
+import GameOfLife from '../src/GameOfLife';
 
 describe('Game Of Life', () => {
-    it('', () => {
+  it('all cells are dead all continues dead in the next generation', () => {
+    const game = new GameOfLife([]);
 
-    })
-} )
+    game.nextGeneration();
+
+    should(game.currentGeneration()).eql([]);
+  });
+});
